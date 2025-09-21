34395bf1fece62d323efc3816875613ccffc9e8c
AungNyanHtet18 <aungnyanh81@gmail.com>
2025-09-21 13:55:44 +0630

    my first commit for this pj

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { AccordionHeader } from "@radix-ui/react-accordion";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./components/ui/accordion";
+
+export default function App() {
+  return (
+    <main className="mt-8 mx-16">
+        <h1 className="text-4xl">Hello Tailwind Css</h1>
+
+        <Accordion type="single" collapsible>
+            {Array.from({length : 4}).map((_, index) => 
+                <AccordionItem key={index} value={`acc-${index+1}`}>
+                    <AccordionTrigger>
+                      <AccordionHeader>{`Accordion ${index + 1}`}</AccordionHeader>
+                    </AccordionTrigger>
+
+                    <AccordionContent>
+                      {`Contents of Accordion ${index + 1}`}
+                    </AccordionContent>
+                </AccordionItem>
+              )}
+        </Accordion>
+    </main>
+  )
+}
